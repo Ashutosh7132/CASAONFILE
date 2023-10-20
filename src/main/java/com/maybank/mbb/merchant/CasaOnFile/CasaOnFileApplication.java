3f3c7d697b3e83a2c42d341c4129baa67320804b
Ashutosh <ashutosh713@outlook.com>
2023-10-20 17:00:16 +0800

    Commit for github

---
+++ src/main/java/com/maybank/mbb/merchant/CasaOnFile/CasaOnFileApplication.java
@@ -0,0 +1,13 @@
+package com.maybank.mbb.merchant.CasaOnFile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaOnFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaOnFileApplication.class, args);
+	}
+
+}
