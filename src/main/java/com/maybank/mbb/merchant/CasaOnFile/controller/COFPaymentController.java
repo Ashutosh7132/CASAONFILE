af31435ee45993bc3592861864b11983f07503e2
Ashutosh <ashutosh713@outlook.com>
2023-10-21 20:39:21 +0800

    2nd Commit

---
+++ src/main/java/com/maybank/mbb/merchant/CasaOnFile/controller/COFPaymentController.java
@@ -0,0 +1,10 @@
+package com.maybank.mbb.merchant.CasaOnFile.controller;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class COFPaymentController {
+}
