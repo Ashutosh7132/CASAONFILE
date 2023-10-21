287c50446938b65c1c6a8e357ade3eb3808e906a
Ashutosh <ashutosh713@outlook.com>
2023-10-22 01:15:06 +0800

    8th Commit

--- src/main/java/com/maybank/mbb/merchant/CasaOnFile/dto/COFPaymentInquiryDto.java
+++ src/main/java/com/maybank/mbb/merchant/CasaOnFile/dto/COFPaymentInquiryDto.java
@@ -13,35 +13,40 @@ public class COFPaymentInquiryDto {
     private String merchantId;
 
     private String merchantTxnRefNo;
+    private String txnFrequency;
 
     private String txnAmount;
     private String txnCurrency;
-    private String txnDescription;
     private Date dateTime = new Date();
 
     private String txnType;
+
+    private String tokenRefNo;
 
     private String nonce;
 
     private byte[] checksum;
 
-    public COFPaymentInquiryDto(String userId, String merchantId, String merchantTxnRefNo, Date dateTime, String txnType, String nonce, byte[] checksum, String txnAmount, String txnCurrency, String txnDescription) throws NoSuchAlgorithmException {
+    public COFPaymentInquiryDto(String userId, String merchantId, String merchantTxnRefNo, String txnFrequency,String txnAmount,String txnCurrency,Date dateTime, String txnType, String tokenRefNo, String nonce, byte[] checksum) throws NoSuchAlgorithmException {
         this.userId = userId;
         this.merchantId = merchantId;
         this.merchantTxnRefNo = merchantTxnRefNo;
-        this.dateTime = dateTime;
-        this.txnType = txnType;
-        this.nonce = nonce;
+        this.txnFrequency = txnFrequency;
         this.txnAmount = txnAmount;
         this.txnCurrency = txnCurrency;
-        this.txnDescription = txnDescription;
+        this.dateTime = dateTime;
+        this.txnType = txnType;
+        this.tokenRefNo = tokenRefNo;
+        this.nonce = nonce;
         this.checksum = calculateChecksum();
     }
 
     private byte[] calculateChecksum() throws NoSuchAlgorithmException {
 
         String data = userId.concat("|").concat(merchantId).concat("|").concat(merchantTxnRefNo)
-                .concat("|").concat(String.valueOf(dateTime)).concat("|").concat(txnType).concat("|").concat(txnAmount).concat("|").concat(txnCurrency).concat("|").concat(txnDescription).concat("|").concat(nonce);
+                .concat("|").concat(txnFrequency).concat("|").concat(txnAmount).concat("|").concat(txnCurrency)
+                .concat("|").concat(String.valueOf(dateTime)).concat("|")
+                .concat(txnType).concat("|").concat(tokenRefNo).concat("|").concat(nonce);
 
         MessageDigest md = MessageDigest.getInstance("SHA-256");
 
