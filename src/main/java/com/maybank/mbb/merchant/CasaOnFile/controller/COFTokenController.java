d7e1d7e0b5065123a53d9729b5f090e543248580
Ashutosh <ashutosh713@outlook.com>
2023-10-21 22:47:24 +0800

    4th Commit

--- src/main/java/com/maybank/mbb/merchant/CasaOnFile/controller/COFTokenController.java
+++ src/main/java/com/maybank/mbb/merchant/CasaOnFile/controller/COFTokenController.java
@@ -2,9 +2,18 @@ package com.maybank.mbb.merchant.CasaOnFile.controller;
 
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/users")
 public class COFTokenController {
+
+    @GetMapping("/hi")
+    public String getHello(){
+
+        return "Hi Ashutosh Shriansh";
+    }
 }
